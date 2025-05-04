b965043c227d65c4cb3059bb314e85ff6df9cb55
João Pedro Paiva <jppaiva1706@gmail.com>
2025-05-03 22:46:29 -0300

    Primeira pergunta

---
+++ chatbot.py
@@ -0,0 +1,7 @@
+# Decidi criar o chatbot do zero sem auxilio de inteligencia artificial
+
+def responder(mensagem):
+    mensagem = mensagem.lower()
+
+    if("line" and "atual" in mensagem):
+        return "Line-up atual: Kscerato, Yuurih, Molodoy, Yekindar e FalleN."
